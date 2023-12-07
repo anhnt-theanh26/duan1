0aeb27e4c2ffb93a0f8f5b55c1c1ba0ccca20f4c
BuiDat2012 <datbxph40919@fpt.edu.vn>
2023-12-07 15:58:17 +0700

    update a file addtocart

--- view/khachhang/sanpham/addtocart.php
+++ view/khachhang/sanpham/addtocart.php
@@ -1,31 +1,42 @@
 <?php
 session_start();
+
+// Kiểm tra xem có tồn tại mảng giỏ hàng hay không.
 if (!isset($_SESSION['giohang'])) {
-    $_SESSION['giohang'];
+    // Nếu không có thì đi khởi tạo
+    $_SESSION['giohang'] = [];
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Lấy dữ liệu từ ajax đẩy lên
     $id = $_POST['id'];
     $name = $_POST['name'];
     $price = $_POST['price'];
 
+    // Kiểm tra sản phẩm đã có trong giỏ hàng chưa
     $index = false;
     if (!empty($_SESSION['giohang'])) {
         $index = array_search($id, array_column($_SESSION['giohang'], 'id'));
     }
+    
+    // array_column() trích xuất một cột từ mảng giỏ hàng và trả về một mảng chứ giá trị của cột id
     if ($index !== false) {
         $_SESSION['giohang'][$index]['quantity'] += 1;
     } else {
+        // Nếu sản phẩm chưa tồn tại thì thêm mới vào giỏ hàng
         $product = [
             'id' => $id,
             'name' => $name,
             'price' => $price,
-            'quantity' => 1,
+            'quantity' => 1
         ];
+        
         $_SESSION['giohang'][] = $product;
+        // var_dump($_SESSION['giohang']);die;
     }
+    // Trả về số lượng sản phẩm của giỏ hàng
     echo count($_SESSION['giohang']);
 } else {
-    echo 'yeeu cau khong hop le';
+    echo 'Yêu cầu không hợp lệ';
 }
 ?>
